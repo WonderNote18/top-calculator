6359aaa77d247f9ea408a1da3a63135703c9e048
WonderNote18 <hector.d.almanza@gmail.com>
2020-05-13 12:34:10 -0500

    Edited index.html to add calculator section Edited style.css to format alignment and spacing. Edited style.css to create border and bg for calculator.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function operate(operator, num1, num2) {
+    if (operator == "add") {
+        add(num1, num2)
+    } else if (operator == 'subtract') {
+        subtract(num1, num2)
+    } else if (operator == "multiply") {
+        multiply(num1, num2)
+    } else if (operator == "divide") {
+        divide(num1, num2)
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
