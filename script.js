64bb541edc54c4a692b92a1a848e74984d2cf659
WonderNote18 <hector.d.almanza@gmail.com>
2020-05-25 22:55:42 -0500

    Added divByZero to check if division is 0/0 Added updateOutputText() to add color and weight to output Remove DOM changes in functions, handled by updateOuputText Added logic in operate() to check if 0/0 flag is raised Added clearData to reset data when "C" pressed.

--- script.js
+++ script.js
@@ -3,16 +3,32 @@ let num1 = Number;
 let num2 = Number;
 let op = '';
 let result = 0;
+let divByZero = false;
+
+
+function updateOutputText(status) {
+    document.getElementById('calcOutput').className = 'calcText ' + status;
+    if (status == 'normal') {
+        document.getElementById('calcOutput').value = output;
+    } else if (status == 'operator') {
+        document.getElementById('calcOutput').value = output + ' ' + op;
+    } else if ((status == 'resultPositive') || (status == 'resultNegative')) {
+        document.getElementById('calcOutput').value = num1 +  ' ' + op + ' ' + num2 + ' = ' + result;
+    } else if (status == 'error') {
+        document.getElementById('calcOutput').value = "E R R O R";
+    }
+}
 
 function addToOutput(char) {
-    if (output.includes(' ')) {output = '0'}
+    if (output.includes(' ')) {output = '0';}
 
     if (output == '0') {
         output = char;
     } else {
         output += char;
     }
-    document.getElementById('calcOutput').value = output;
+
+    updateOutputText('normal');
 }
 
 function assignOperator(operator) {
@@ -21,14 +37,12 @@ function assignOperator(operator) {
     } else if (typeof(num2) == 'function') {
         operate(op, num1, num2);
     }
-    output += ' ' + operator;
-    document.getElementById('calcOutput').value = output;
     op = operator;
+    output += ' ' + operator;
+    updateOutputText('operator');
 }
 
 function operate(operator, num1, num2) {
-    // TODO: Output shows 'num1 op num2 = result'
-
     num2 = parseFloat(output);
     if (operator == "+") {
         add(num1, num2);
@@ -38,10 +52,32 @@ function operate(operator, num1, num2) {
         multiply(num1, num2);
     } else if (operator == "/") {
         divide(num1, num2);
+    } else if (operator == "C") {
+        clearData();
     }
     
-    output = result.toString()
-    document.getElementById('calcOutput').value = output;
+    if (!divByZero) {
+        if (result >= 0) {
+            updateOutputText('resultPositive');
+        } else {
+            updateOutputText('resultNegative');
+        }
+        
+    } else {
+        divByZero = false;
+        clearData();
+        updateOutputText('error');
+    }
+}
+
+function clearData() {
+    let output = '0';
+    let num1 = Number;
+    let num2 = Number;
+    let op = '';
+    let result = 0;
+    let divByZero = false;
+    updateOutputText('normal');
 }
 
 function add(num1, num2) {
@@ -57,5 +93,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    result = num1 / num2;
+    if ((num1 == 0) && (num2 == 0)) {
+        divByZero = true;
+    } else {
+        result = num1 / num2;
+    }
 }
