73a0309826b321ea2474c07462d7342fe2545d30
WonderNote18 <hector.d.almanza@gmail.com>
2020-05-25 19:56:39 -0500

    Add global vars in script.js to handle logic Changed addToOutput to check if output is num Add assignOperator to set op and prepare/complete operate() Edited math functions to support page changes with result var

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-let calcOutput = '0';
+let output = '0';
+let num1 = Number;
+let num2 = Number;
+let op = '';
+let result = 0;
 
 function addToOutput(char) {
-    if (calcOutput == '0') {
-        calcOutput = char;
+    if (output.includes(' ')) {output = '0'}
+
+    if (output == '0') {
+        output = char;
     } else {
-        calcOutput += char;
+        output += char;
     }
-    document.getElementById('calcOutput').value = calcOutput
+    document.getElementById('calcOutput').value = output;
+}
+
+function assignOperator(operator) {
+    if (typeof(num1) == 'function') {
+        num1 = parseFloat(output);
+    } else if (typeof(num2) == 'function') {
+        operate(op, num1, num2);
+    }
+    output += ' ' + operator;
+    document.getElementById('calcOutput').value = output;
+    op = operator;
 }
 
 function operate(operator, num1, num2) {
-    if (operator == "add") {
-        add(num1, num2)
-    } else if (operator == 'subtract') {
-        subtract(num1, num2)
-    } else if (operator == "multiply") {
-        multiply(num1, num2)
-    } else if (operator == "divide") {
-        divide(num1, num2)
+    // TODO: Output shows 'num1 op num2 = result'
+
+    num2 = parseFloat(output);
+    if (operator == "+") {
+        add(num1, num2);
+    } else if (operator == '-') {
+        subtract(num1, num2);
+    } else if (operator == "*") {
+        multiply(num1, num2);
+    } else if (operator == "/") {
+        divide(num1, num2);
     }
+    
+    output = result.toString()
+    document.getElementById('calcOutput').value = output;
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    result = num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    result = num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    result = num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    result = num1 / num2;
 }
